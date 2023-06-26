3f089c56e14881dc53b2b114a4e17f47a0923bc8
Vinícius Nathan <viniciosnathan@gmail.com>
2023-06-26 15:59:30 -0300

    Alteracoes nos metodos de validacao

--- Projeto/src/Validacao.java
+++ Projeto/src/Validacao.java
@@ -1,3 +1,4 @@
+
 public class Validacao {
     private String codigo;
     private String tipo; //define a operacao de acordo com classe
@@ -13,7 +14,7 @@ public class Validacao {
         this.codigo = revista.getIssn();
     }
 
-     Validacao(Artigo artigo){
+    Validacao(Artigo artigo){
         this.tipo = "Artigo";
         this.codigo = artigo.getDoi();
     }
@@ -22,7 +23,7 @@ public class Validacao {
         this.tipo = "CPF";
         this.codigo = cpf;
     }
-
+	
     //Getters e Setters
     public String getCodigo() {
 		return codigo;
@@ -39,165 +40,178 @@ public class Validacao {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-
-    //Desambiguacao para verificar validade de cada tipo
+	
+	//Desambiguacao para verificar validade de cada tipo
     public boolean validarCodigo(){
         switch(this.tipo){
             case "Livro":
-            return validarLivro(this.codigo);
+            	return verificarISBN(this.codigo);
             case "Revista":
-            return validarRevista(this.codigo);
+            	return verificarISSN(this.codigo);
             case "Artigo":
-            return validarArtigo(this.codigo);
+            	return verificarDOI(this.codigo);
             case "CPF":
             {
-            boolean valido = validarCPF(this.codigo);
-            if (!valido)
-                System.out.println("Credenciais inválidas. . . Tente novamente");
-            return valido;
+	            boolean valido = validarCPF(this.codigo);
+	            if (!valido) {
+	                System.out.println("Credenciais inválidas. . . Tente novamente");
+	            }
+	            return valido;
             }
             default:
             return false;
         }
     }
-
-    //Verifica a validade do isbn
-    public boolean validarLivro(String codigo){
-        //Remover caracteres nao numericos
-        String isbn = codigo.replaceAll("[^0-9,X]", "");
-        //Verificar se contem 10 ou 13 digitos
-        if (isbn.length() == 10)
-            return algoritmoIsbn2001(isbn);
-        else if (isbn.length() == 13)
-            return algoritmoIsbn2005(isbn);
-        return false;
-    }
-
-    //Verifica a validade do issn
-    public boolean validarRevista(String codigo){
-        //Remover caracteres nao numericos
-        String issn = codigo.replaceAll("[^0-9,X]", "");
-        //Verificar se contem 8 digitos
-        if (issn.length() == 8)
-            return algoritmoIssn(issn);
-        return false;
-    }
-
-    //Verifica formato do Doi
-    public boolean validarArtigo(String codigo){
-        String[] doi = codigo.split("/"); // Divide a string codigo
-        // Extrai os valores do array e separa o prefixo
-        String prefixo = doi[0];
-        //O guia oficial DOI indica que os mesmos devem estar no formato "doi:10.nnnn[...]/[sufixo]", com o numero de 'n's >= 4
-        if (prefixo.length() < 11)
-            return false;
-        else if (prefixo.charAt(4) != '1' || prefixo.charAt(5) != '0')
-            return false;
-        return true;
-    }
-
-    //Verifica validade do CPF de Usuario
-    public boolean validarCPF(String cpf){
-        //Remover Caracteres Nao Numericos
-		String numeros = cpf.replaceAll("[^0-9]", "");
-		//Verificar Se Contem 11 Digitos 
-		if (numeros.length() != 11)
-			return false;
-		//Verificar Se Todos os Digitos Sao Iguais
-		if (repetidos(numeros))
-			return false;
-		//Calculando Algoritmo de Verificacao
-		return algoritmoCPF(numeros);
-		}
 	
-    //Verifica se todos os digitos numéricos são idênticos
-	public boolean repetidos(String numeros) {
-		int l = numeros.length();
-		for (int i = 1; i < l; i++)
-			if (numeros.charAt(0) != numeros.charAt(i))
-				return false;
-		return true;
+    
+	// Método para validar o ISBN 
+	public static boolean verificarISBN(String isbn) {
+	    // Removendo hífens e espaços em branco do ISBN
+	    String isbnLimpo = isbn.replaceAll("-", "").replaceAll("\\s+", "");
+
+	    // Verificando se o ISBN possui 10 ou 13 dígitos
+	    if (isbnLimpo.length() != 10 && isbnLimpo.length() != 13) {
+	        return false;
+	    }
+
+	    // Realizando a verificação do dígito verificador
+	    int soma = 0;
+	    int multiplicador = 10;
+	    for (int i = 0; i < isbnLimpo.length() - 1; i++) {
+	        char c = isbnLimpo.charAt(i);
+	        if (!Character.isDigit(c)) {
+	            return false;
+	        }
+	        int digito = Character.getNumericValue(c);
+	        soma += digito * multiplicador;
+	        multiplicador--;
+	    }
+
+	    int digitoVerificador = Character.getNumericValue(isbnLimpo.charAt(isbnLimpo.length() - 1));
+
+	    if (isbnLimpo.length() == 10) {
+	        return (soma % 11 == digitoVerificador);
+	    } else { // ISBN com 13 dígitos
+	        return ((soma % 10 == 0 && digitoVerificador == 0) || (10 - (soma % 10) == digitoVerificador));
+	    }
 	}
-		
-	//Verifica se 10o e 11o digitos são condizentes com os 9 primeiros
-    public boolean algoritmoCPF(String numeros) {
-		//Verificando 10o Digito
-		int soma = 0, mult = 10;
-		for (int i = 0; i < 9; i++) //Subtrai 48 de acordo com tabela ASCII 
-			soma += (mult - i) * (numeros.charAt(i) - 48); //multiplica 1os 9 digitos por multiplicador descendo de 10-2 
-		int resto = (soma % 11); //resto da soma por 11
-		if (resto <= 2)
-			if (numeros.charAt(9) - 48 != 0) //se resto é menor ou igual do que 2, 10o digito deve ser 0
-				return false;
-		else if (numeros.charAt(9) - 48 != 11 - resto) //se resto é maior do que 2, 10o digito deve ser 11 menos resto
-			return false;
-			
-		//Verificando 11o Digito
-		soma = 0;
-        mult = 11;
-		for (int i = 0; i < 10; i++) //Mesmo processo, porem agora indo ate o 10o digito e multiplicador desce de 11-2
-			soma += (mult - i) * (numeros.charAt(i) - 48);
-		resto = (soma % 11);
-		if (resto <= 2)
-			return (numeros.charAt(10) - 48 == 0);
-		else
-            return (numeros.charAt(10) - 48 == 11 - resto);
-    }
 
-    //Verifica se 10o digito condiz com 9 primeiros
-    public boolean algoritmoIsbn2001(String isbn){
-        //Soma dos 9 primeiros digitos multiplicados pela posição da esquerda para a direita. O módulo 11 da soma deve ser 0
-        int soma = 0, checkdigit;
-        if (isbn.charAt(9) == 'X')
-            checkdigit = 10;
-        else
-            checkdigit = isbn.charAt(9) - 48;
-        for (int i = 0; i < 9; i++)
-            soma += (i + 1) * (isbn.charAt(i) - 48);
-        int resto = (soma % 11);
-        if (resto == 0)
-            return (checkdigit == 0);
-        else
-            return (checkdigit == 11 - resto);
-    }
+	// Método para validar o ISSN 
+	public static boolean verificarISSN(String issn) {
+	    // Removendo hífens e espaços em branco do ISSN
+	    String issnLimpo = issn.replaceAll("-", "").replaceAll("\\s+", "");
 
-      //Verifica se 13o digito condiz com 12 primeiros
-      public boolean algoritmoIsbn2005(String issn){
-        //Multiplica alternadamente cada um dos 12 digitos por 1 ou 3. O módulo 10 da soma total deve ser 0
-        int soma = 0, mult = 1, checkdigit;
-        if (issn.charAt(12) == 'X')
-            checkdigit = 10;
-        else
-            checkdigit = issn.charAt(12) - 48;
-        for (int i = 0; i < 12; i++){
-            soma += mult * (issn.charAt(i) - 48);
-            if (mult == 1)
-                mult = 3;
-            else
-                mult = 1;
+	    // Verificando se o ISSN possui 8 dígitos
+	    if (issnLimpo.length() != 8) {
+	        return false;
+	    }
+
+	    // Realizando a verificação do dígito verificador
+	    int soma = 0;
+	    int multiplicador = 8;
+	    for (int i = 0; i < issnLimpo.length() - 1; i++) {
+	        char c = issnLimpo.charAt(i);
+	        if (!Character.isDigit(c)) {
+	            return false;
+	        }
+	        int digito = Character.getNumericValue(c);
+	        soma += digito * multiplicador;
+	        multiplicador--;
+	    }
+
+	    int digitoVerificador = Character.getNumericValue(issnLimpo.charAt(issnLimpo.length() - 1));
+
+	    int resto = soma % 11;
+	    int resultado = (resto == 0) ? 0 : 11 - resto;
+
+	    return (resultado == digitoVerificador);
+	}
+
+	// Método para validar o DOI 
+	public static boolean verificarDOI(String doi) {
+	    // Removendo espaços em branco do DOI
+	    String doiLimpo = doi.replaceAll("\\s+", "");
+
+	    // Verificando se o DOI começa com "10." (padrão de prefixo)
+	    if (!doiLimpo.startsWith("10.")) {
+	        return false;
+	    }
+
+	    // Verificando se há pelo menos um caractere após o prefixo
+	    if (doiLimpo.length() <= 3) {
+	        return false;
+	    }
+
+	    // Verificando se o DOI contém um "/" separando o prefixo do sufixo
+	    int separadorIndex = doiLimpo.indexOf("/");
+	    if (separadorIndex <= 3 || separadorIndex == doiLimpo.length() - 1) {
+	        return false;
+	    }
+
+	    // Verificando se o prefixo e o sufixo são alfanuméricos
+	    String prefixo = doiLimpo.substring(0, separadorIndex);
+	    String sufixo = doiLimpo.substring(separadorIndex + 1);
+	    if (!prefixo.matches("[A-Za-z0-9]+") || !sufixo.matches("[A-Za-z0-9]+")) {
+	        return false;
+	    }
+
+	    // Verificando se o DOI contém pelo menos um ponto no sufixo
+	    if (!sufixo.contains(".")) {
+	        return false;
+	    }
+
+	    // Verificação concluída, o DOI é considerado válido
+	    return true;
+	}
+	
+	
+	// Método para validar o CPF
+    public static boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("\\D+", ""); // Removendo caracteres não numéricos do CPF
+        if (cpf.length() != 11) { // Verificando se o CPF tem 11 dígitos
+            return false;
         }
-        int resto = (soma % 10);
-        if (resto == 0)
-            return (checkdigit == 0);
-        else
-            return (checkdigit == 10 - resto);
-    }
+        // Verificando se todos os dígitos são iguais
+        boolean todosDigitosIguais = true;
+        for (int i = 1; i < cpf.length(); i++) {
+            if (cpf.charAt(i) != cpf.charAt(0)) {
+                todosDigitosIguais = false;
+                break;
+            }
+        }
+        if (todosDigitosIguais) {
+        	System.out.println("O CPF fornecido pelo cliente é inválido");
+            return false;
+        }
+        // Calculando os dígitos verificadores
+        int soma = 0;
+        int peso = 10;
+        for (int i = 0; i < 9; i++) {
+            int digito = Character.getNumericValue(cpf.charAt(i));
+            soma += digito * peso;
+            peso--;
+        }
+        int resto = soma % 11;
+        int primeiroDigitoVerificador = (resto < 2) ? 0 : (11 - resto);
 
-    //Verifica se 8o digito condiz com 7 primeiros
-    public boolean algoritmoIssn(String issn){
-        //Soma dos oito digitos multiplicados pela posição da direita para a esquerda. O módulo 11 da soma deve ser 0
-        int soma = 0, mult = 8, checkdigit;
-        if (issn.charAt(7) == 'X')
-            checkdigit = 10;
-        else
-            checkdigit = issn.charAt(7) - 48;
-        for (int i = 0; i < 7; i++)
-            soma += (mult - i) * (issn.charAt(i) - 48);
-        int resto = (soma % 11);
-        if (resto == 0)
-            return (checkdigit == 0);
-        else
-            return (checkdigit == 11 - resto);
-    }
+        soma = 0;
+        peso = 11;
+        for (int i = 0; i < 10; i++) {
+            int digito = Character.getNumericValue(cpf.charAt(i));
+            soma += digito * peso;
+            peso--;
+        }
+        resto = soma % 11;
+        int segundoDigitoVerificador = (resto < 2) ? 0 : (11 - resto);
 
+        // Verificando se os dígitos verificadores calculados são iguais aos dígitos verificadores do CPF
+        if (primeiroDigitoVerificador == Character.getNumericValue(cpf.charAt(9)) &&
+                segundoDigitoVerificador == Character.getNumericValue(cpf.charAt(10))) {
+            return true;
+        } else {
+        	System.out.println("O CPF fornecido pelo cliente é inválido");
+            return false;
+        }
+    }
+	
 }
